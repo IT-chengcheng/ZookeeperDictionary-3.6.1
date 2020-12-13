5278dc511bb8568b11e38e0edd8b0c48c6f92839
张呈成 <360467435@qq.com>
2020-12-13 22:09:20 +0800

    zk -> 分布式配置中心

--- luban-vip/src/main/java/com/luban/distributed_lock/Order.java
+++
@@ -1,8 +0,0 @@
-package com.luban.distributed_lock;
-
-public class Order {
-
-    public void createOrder() {
-        System.out.println(Thread.currentThread().getName() + "创建order");
-    }
-}
--- luban-vip/src/main/java/com/luban/distributed_lock/Pay.java
+++
@@ -1,8 +0,0 @@
-package com.luban.distributed_lock;
-
-public class Pay {
-
-    public void pay() {
-        System.out.println(Thread.currentThread().getName() + "支付成功");
-    }
-}
